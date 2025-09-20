d93b2a52185316243e2c27bcee1ee1d9e89c8343
sagar katoch <katochsagar2@gmail.com>
2025-09-20 20:27:38 +0530

    added some sample mongo data

--- models/listings.js
+++ models/listings.js
@@ -12,7 +12,8 @@ required:true,
         type:String,
         
     },
-   image:{type:String,
+   image:{
+    type:String,
     default:"https://plus.unsplash.com/premium_photo-1661876449499-26de7959878f?q=80&w=1674&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set:(v)=>v===""?"https://plus.unsplash.com/premium_photo-1661876449499-26de7959878f?q=80&w=1674&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v,
    },
