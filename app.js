506090d47d680463a9dabf384421fcf276e4fa29
sagar katoch <katochsagar2@gmail.com>
2025-09-21 00:32:11 +0530

    added some routes

--- app.js
+++ app.js
@@ -1,40 +1,77 @@
-const express= require("express");
-const app= express();
-const mongoose= require("mongoose");
-const Listing= require("./models/listings.js")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listings.js");
 
-main().then(()=>{
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
+
+main().then(() => {
     console.log("succesfully connected to the database");
-})
-.catch(err => console.log(err));
+}).catch(err => console.log(err));
+
+
+
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("hi i am root");
 })
 
-app.get("/testListing",async (req,res)=>{
+// Index-route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
 
-let sampleListing = new Listing({
-    title:"My Villa",
-    description:"By the Beach",
-    price:10000,
-    location:"Calangute,Goa",
-    country:"India"
+    
 })
 
-await sampleListing.save();
-console.log("sample was saved");
-res.send("successfull testing ");
+// show route
+app.get("/listings/:id",async (req,res)=>{
 
+let {id}= req.params;
+const listing=await Listing.findById(id)
+res.render("listings/show.ejs",{listing});
 })
-app.listen(8080,()=>{
+
+
+// app.get("/testListing",async (req,res)=>{
+
+// let sampleListing = new Listing({
+//     title:"My Villa",
+//     description:"By the Beach",
+//     price:10000,
+//     location:"Calangute,Goa",
+//     country:"India"
+// })
+
+// await sampleListing.save();
+// console.log("sample was saved");
+// res.send("successfull testing ");
+
+// })
+
+app.listen(8080, () => {
     console.log("server succesfully running on the port No.8080");
 
 })
+
+
+
+
+
+
+
+
+
