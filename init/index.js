c47ad8c6cb4d0f4edd827462e9518090bf9e37a4
sagar katoch <katochsagar2@gmail.com>
2025-09-20 20:25:49 +0530

    added new data

---
+++ init/index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const initData= require("./data.js");
+
+const Listing= require("../models/listings.js");
+
+main().then(()=>{
+    console.log("succesfully connected to the database");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+
+const initDB=async ()=>{
+await Listing.deleteMany({});
+await Listing.insertMany(initData.data);
+console.log("data was initialized");
+
+}
+initDB();
